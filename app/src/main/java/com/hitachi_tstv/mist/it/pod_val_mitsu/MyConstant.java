3cda8ff0e7f41e346179af178613eaffb939b038
Vipavee <vi_vpk_4ever@hotmail.com>
2017-09-21 11:24:38 +0700

    DateDeliveryAvtivity 21/09/2017 11:25

---
+++ app/src/main/java/com/hitachi_tstv/mist/it/pod_val_mitsu/MyConstant.java
@@ -0,0 +1,14 @@
+package com.hitachi_tstv.mist.it.pod_val_mitsu;
+
+/**
+ * Created by Tunyaporn on 9/18/2017.
+ */
+
+public interface MyConstant {
+    public static final String projectString = "MMTH";
+    public static final String serverString = "http://service.eternity.co.th/";
+    public static final String pathString = "/app/CenterService/";
+    public static final String urlGetUserLogin = serverString + projectString + pathString + "getUserLogin.php";
+    public static final String urlGetPlanDate = serverString + projectString + pathString +"getPlanDate.php";
+
+}
